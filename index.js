73b140516b8d217eecde0929fb40093840c9a561
cecilia <leececilia14@gmail.com>
2021-07-28 17:05:28 +0900

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(collection, name){
+    return collection.filter(function(driverName){
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(collection, nameCharacters){
+  let nameLength= nameCharacters.length;
+  return collection.filter(function(driverName){
+      return driverName.slice(0, nameLength)=== nameCharacters;
+  });
+}
+
+function matchName(collection, string){
+    return collection.filter(function(driver){
+        return driver.name.toLowerCase()=== string.toLowerCase();
+    });
+}
